7e4359602c2c51a66afde6e3b5d14b3b1c5063ce
vichovalderas <45319583+vichovalderas@users.noreply.github.com>
2025-07-02 00:19:00 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+export const dynamic = 'force-dynamic';
 import { useEffect } from "react"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Alert, AlertDescription } from "@/components/ui/alert"
